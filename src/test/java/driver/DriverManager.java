a23f16b6832b500b56d732680f87e6077a07558c
abhishek kumar <abhishek766801.as@gmail.com>
2025-02-16 17:27:48 +0530

    first commit

---
+++ src/test/java/driver/DriverManager.java
@@ -0,0 +1,40 @@
+package driver;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class DriverManager {
+	public static WebDriver driver;
+	
+	
+	 Actions action = new Actions(driver);
+	// Actions actionChain = new ActionChain(driver);
+	
+	public static WebDriver getDriver() {
+		if(driver==null) {
+			driver=new ChromeDriver();
+
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			
+		}
+		return driver;
+		
+	}
+	public static void quitDriver() {
+		if(driver!=null)
+		{
+			driver.quit();
+			driver=null;
+		}
+		
+	}
+	
+	
+}
